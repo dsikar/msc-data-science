a07eac6e08419960ab073f2c3ef18347667ae9e6
Daniel Sikar <dsikar@gmail.com>
2018-10-07 21:09:26 +0100

    Week 2 Python programming

--- INM430/Week2/INM439-LAB-W2-Ex3.py
+++ INM430/Week2/INM439-LAB-W2-Ex3.py
@@ -1,3 +1,7 @@
 import pandas as pd
+# import numpy as np
 tb = pd.read_csv("TB_burden_countries_2014-09-29.csv")
-tb.head()
+tbNamibia = tb[tb['country'] == 'Namibia'].head(5)
+tbAfghanistan = tb[tb['country'] == 'Afghanistan'].head(5)
+tbcopy = tbNamibia.append(tbAfghanistan, ignore_index=True)
+tbcopy
