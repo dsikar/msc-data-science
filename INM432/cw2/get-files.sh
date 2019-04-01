00ba9235c82ffde13eb08bf85832588792488ae3
dsikar <dsikar@gmail.com>
2019-04-01 17:04:24 +0100

    File download script.

---
+++ INM432/cw2/get-files.sh
@@ -0,0 +1,4 @@
+  wget https://storage.cloud.google.com/cloud-ml-data/img/flower_photos/all_data.csv
+  wget https://storage.cloud.google.com/cloud-ml-data/img/flower_photos/train_set.csv
+  wget https://storage.cloud.google.com/cloud-ml-data/img/flower_photos/eval_set.csv
+  wget https://storage.cloud.google.com/cloud-ml-data/img/flower_photos/dict.txt
