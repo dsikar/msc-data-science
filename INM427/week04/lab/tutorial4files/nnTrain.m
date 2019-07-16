cd930d52fe77b8a8e298d116004a1a2bfd901078
jsikard <jsikard@jci.com>
2019-07-16 16:31:35 +0100

    Added comments to nnTrain.m

---
+++ INM427/week04/lab/tutorial4files/nnTrain.m
@@ -0,0 +1,281 @@
+close all; clear all; clc;
+
+[folder, name, ext] = fileparts(which('nnTrain'));
+cd(folder);
+
+% Adding necessary files to the java path
+javaaddpath([folder '\weka.jar']);
+javaaddpath([folder '\SMOTE.jar']);
+
+% Preparing the data
+% Open file promoter.csv, start reading rows and columns with no offset
+% (0,0) i.e. no column headers or row labels
+file = 'promoter';
+X = csvread([file '.csv'], 0, 0);
+% size(X) returns size of array X
+% 106   229
+% i.e. 106 rows and 229 columns, which could also be expressed as
+% size(X, 1) - number of rows in array X
+% size(X, 2) - number of columns in array X
+% The expression X(:,size(X,2)) says; w.r.t. array X, given me all the rows
+% this is the colon on left of comma for the last column, that is to say
+% column indexed by index size(X,2) ~ 229
+% We could also say X(1:1,size(X,2)) to get the first row of the last
+% column or X(1:2,1:2) to get the first two rows of the first two columns
+t = X(:,size(X,2));
+% Next we assign empty array [] to the last column which is to remove last
+% column. Remember number of columns is 229
+X(:,size(X,2)) = [];
+% Now size(X,2)is 228, So we have an input vector X, with 116 rows and 228
+% columns, and a target vector t with 116 rows and 1 column.
+% size(t) - 106     1 , size(X) 106   228
+Type = 'scaling';
+
+%This function normalizes each column of an array  using the standard score or feature scaling.
+X = StatisticalNormaliz(X,Type);
+X = (X * 2) - 1; 
+X = X';
+
+E = 1;
+originalInputSize = size(X,2);
+classes = [1 -1];
+
+% Get generator
+st = rand('state');
+                                                                                                                                                         
+% Network Init
+K = 2; % Number of Layers
+etaInit = 0.01; % Learning Rate Initial Value
+Delta = 0.1; % Stop Criterion #1
+theta = 0.01; % Stop Criterion #2
+numUpdates = 10000; % Max number of epochs
+N = size(X,2); % Number of Input Vectors
+h = 11; % Number of Hidden Neurons
+alpha = 0.999; % Learning Rate Decay Factor
+mu = 0.1; % Momentum constant;
+M = 10; % Number of Folds (N = leave-one-out)
+shuffle = 1; % Shuffle control (turn it off for reproductibility)
+smote = 0; % SMOTE oversampling
+
+if(smote),
+    smoteStr = 'yes';
+else
+    smoteStr = 'no';
+end;
+    
+fprintf('----------------------------\n');
+fprintf('- Neural Networks Training -\n');
+fprintf('----------------------------\n\n');
+
+fprintf('Dataset: %s\n', file);
+
+% Setting SMOTE up
+oldInputSize = N;
+if(smote),
+    data = [X ; t']';
+
+    data2arff(data, file);
+
+    filter = javaObjectEDT('weka.filters.supervised.instance.SMOTE');
+
+    fileReader = javaObjectEDT('java.io.FileReader',[file '.arff']);
+    buffReader = javaObjectEDT('java.io.BufferedReader', fileReader);
+    arffReader = javaObjectEDT('weka.core.converters.ArffLoader$ArffReader', buffReader);
+                
+    instances = arffReader.getData();
+    instances.setClassIndex(size(data, 2)-1);
+
+    paramFilter = java.lang.String('-C 0 -K 5 -P 100.0 -S 123');
+    paramFilter = paramFilter.split(' ');
+
+    filter.setOptions(paramFilter);
+    filter.setInputFormat(instances);
+    result = filter.useFilter(instances, filter);
+
+    filteredData = zeros(result.numAttributes(),result.numInstances());
+    for i = 1:result.numAttributes(),
+        filteredData(i,:) = result.attributeToDoubleArray(i-1);
+    end;
+
+    t = (filteredData(size(filteredData, 1),:)*2)-1;
+    X = filteredData;
+    X(size(filteredData, 1),:) = [];
+    
+    if(M == N),
+        N = size(X,2); 
+        M = N;
+    else
+        N = size(X,2);
+    end;  
+end;
+
+fprintf('Training type: %d folds on %d examples\n', M, oldInputSize);
+fprintf('Data balancing? %s\n\n', smoteStr);
+
+[numAttr,numExamples] = size(X);
+
+% Shuffling data
+if (shuffle),
+    X = [X ; t' ; randn(1,numExamples)]';
+    X = sortrows(X,numAttr+2)';
+    t = X(numAttr+1,:); 
+    X = X(1:numAttr,:);
+end;
+
+% M-fold cross validation
+answer = zeros(M, floor(N/M));
+target = zeros(M, floor(N/M));
+
+tp = zeros(1, M);
+fp = zeros(1, M);
+tn = zeros(1, M);
+fn = zeros(1, M);
+acc = zeros(1, M);
+
+avgAcc = 0;
+
+for m = 1:M,
+    
+    if(smote && m <= oldInputSize),
+        fprintf('Training fold %d/%d...\n', m, oldInputSize);
+    else
+        if(m <= oldInputSize),
+            fprintf('Training fold %d/%d...\n', m, M);
+        end;
+    end;
+    
+    % Layers initialization - for multi-class learning, this should be
+    % changed
+    L(1).W = (rand(h,numAttr)-0.5)*0.2;
+    L(1).b = (rand(h,1)-0.5)*0.2;
+    L(2).W = (rand(1,h)-0.5)*0.2;
+    L(2).b = (rand(1,1)-0.5)*0.2;
+
+    for k = 1:K,
+        L(k).vb = zeros(size(L(k).b));
+        L(k).vW = zeros(size(L(k).W));
+    end;
+
+    % Sequential Error Backpropagation Training
+    n = 1; i = 1; finish = 0; eta = etaInit;
+    round = 1; A(m,round) = 0;
+    while not(finish),
+        
+        % Checking if it is a fold example
+        ignoreTraining = 0; ignoreTesting = 0;
+        if ((n > ((m-1)*floor(N/M))) && (n <= (m*floor(N/M)))),
+           ignoreTraining = 1;
+           if((n > N) && ~shuffle),
+               ignoreTesting = 1;
+               break;
+            end;
+        end;
+        
+        J(m, i) = 0;
+        if(~ignoreTraining),
+            for(k = 1:K),
+                L(k).db = zeros(size(L(k).b));
+                L(k).dW = zeros(size(L(k).W));
+            end;
+        end;
+
+        for(ep = n:(n+E-1)),
+            
+            if((ep > N) || ignoreTraining),
+                break;
+            end;
+            
+            % Feed-Forward         
+            L(1).x = X(:,ep);
+            for k = 1:K,
+                L(k).u = L(k).W * L(k).x + L(k).b;
+                L(k).o = tanh(L(k).u);
+                L(k+1).x = L(k).o;
+            end; 
+            e = t(n) - L(K).o;
+            J(m,i) = J(m,i) + (e'*e)/2;
+
+            % Error Backpropagation
+            L(K+1).alpha = e; 
+            L(K+1).W = eye(length(e));
+            for k = fliplr(1:K),
+                L(k).M = eye(length(L(k).o)) - diag(L(k).o)^2;
+                L(k).alpha = L(k).M*L(k+1).W'*L(k+1).alpha;
+                L(k).db = L(k).db + L(k).alpha;
+                L(k).dW = L(k).dW + kron(L(k).x',L(k).alpha);
+            end;
+        end;
+
+        % Updates
+        for k = 1:K,
+            if(ignoreTraining), 
+                break; 
+            end;
+            L(k).vb = eta*L(k).db + mu*L(k).vb;
+            L(k).b = L(k).b + L(k).vb;
+            L(k).vW = eta*L(k).dW + mu*L(k).vW;
+            L(k).W = L(k).W + L(k).vW;
+        end;
+
+        if(~ignoreTraining),
+            A(m,round) = A(m,round) + (J(m, i)/(N-1));
+            J(m, i) = J(m, i)/E;
+        end;
+            
+        % Stop criterion
+        if ((i > 1) && (n == N)),
+            if (((A(m,round) < Delta) && ((round > 2) && (abs(A(m,round-2)-A(m,round-1) < theta) && (abs(A(m,round-1)-A(m,round)) < theta)))) || (i > numUpdates)),
+                finish = 1;
+            end;
+        end;
+        if not(finish)
+            i = i+1; n = n+1; 
+            if n > N, 
+                n = 1;
+                round = round + 1;
+                A(m,round) = 0;
+            end; 
+            eta = eta*alpha;
+        end;
+
+    end;
+
+    % Test
+    % fprintf('Testing fold %d/%d...\n', m, M);
+    
+    if(~ignoreTesting),
+        index = 0;
+        for n = ((m-1)*floor(N/M))+1:m*floor(N/M),      
+            index = index + 1;
+            L(1).x = X(:,n);
+            for k = 1:K,
+                L(k).u = L(k).W*L(k).x + L(k).b;
+                L(k).o = tanh(L(k).u);
+                L(k+1).x = L(k).o;
+            end;
+            answer(m,index) = L(K).o;
+            target(m,index) = t(n);
+
+            if abs(answer(m,index) - target(m,index)) < (abs(classes(2) - classes(1))/2),
+                if(abs(answer(m,index)-classes(1)) < abs(answer(m,index)-classes(2))), %TP case
+                    tp(m) = tp(m) + 1; %TP case
+                else                     
+                    tn(m) = tn(m) + 1; %TN case
+                end;
+            else
+                if(answer(m,index) > 0), %FP case
+                    fp(m) = fp(m) + 1;
+                else                     %FN case
+                    fn(m) = fn(m) + 1;
+                end;
+            end;
+        end;
+    end;
+        
+    acc(m) = acc(m) + ((tp(m) + tn(m))/(tp(m) + tn(m) + fp(m) + fn(m)));
+    avgAcc = avgAcc + (acc(m)/M);
+
+end;
+
+fprintf('Average accuracy: %f\n\n', avgAcc);
+    
